c679e45eed176a990a4bc1324b8c0e2702fe37be
wtvemac <wtvemac@proton.me>
2024-05-05 04:55:44 -0400

    Flash and machine config changes

--- src/devices/machine/intelfsh.h
+++ src/devices/machine/intelfsh.h
@@ -140,6 +140,12 @@ public:
 	fujitsu_29lv800b_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock = 0);
 };
 
+class fujitsu_29f400t_16bit_device : public intelfsh16_device
+{
+public:
+	fujitsu_29f400t_16bit_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock = 0);
+};
+
 class atmel_29c010_device : public intelfsh8_device
 {
 public:
@@ -188,6 +194,12 @@ public:
 	amd_29lv200t_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock = 0);
 };
 
+class amd_29lv800b_16bit_device : public intelfsh16_device
+{
+public:
+	amd_29lv800b_16bit_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock = 0);
+};
+
 class sharp_lh28f016s_device : public intelfsh8_device
 {
 public:
@@ -224,10 +236,10 @@ public:
 	macronix_29lv160tmc_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock = 0);
 };
 
-class macronix_16161616_device : public intelfsh16_device
+class macronix_29F1610_16bit_device : public intelfsh16_device
 {
 public:
-	macronix_16161616_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock = 0);
+	macronix_29F1610_16bit_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock = 0);
 };
 
 class panasonic_mn63f805mnp_device : public intelfsh8_device
@@ -359,49 +371,51 @@ public:
 
 
 // device type definition
-DECLARE_DEVICE_TYPE(INTEL_28F016S5,        intel_28f016s5_device)
-DECLARE_DEVICE_TYPE(SHARP_LH28F016S,       sharp_lh28f016s_device)
-DECLARE_DEVICE_TYPE(SHARP_LH28F016S_16BIT, sharp_lh28f016s_16bit_device)
-DECLARE_DEVICE_TYPE(ATMEL_29C010,          atmel_29c010_device)
-DECLARE_DEVICE_TYPE(AMD_29F010,            amd_29f010_device)
-DECLARE_DEVICE_TYPE(AMD_29F040,            amd_29f040_device)
-DECLARE_DEVICE_TYPE(AMD_29F080,            amd_29f080_device)
-DECLARE_DEVICE_TYPE(AMD_29F400T,           amd_29f400t_device)
-DECLARE_DEVICE_TYPE(AMD_29F800T,           amd_29f800t_device)
-DECLARE_DEVICE_TYPE(AMD_29F800B_16BIT,     amd_29f800b_16bit_device)
-DECLARE_DEVICE_TYPE(AMD_29LV200T,          amd_29lv200t_device)
-DECLARE_DEVICE_TYPE(FUJITSU_29F160TE,      fujitsu_29f160te_device)
-DECLARE_DEVICE_TYPE(FUJITSU_29F016A,       fujitsu_29f016a_device)
-DECLARE_DEVICE_TYPE(FUJITSU_29DL164BD,     fujitsu_29dl164bd_device)
-DECLARE_DEVICE_TYPE(FUJITSU_29LV002TC,     fujitsu_29lv002tc_device)
-DECLARE_DEVICE_TYPE(FUJITSU_29LV800B,      fujitsu_29lv800b_device)
-DECLARE_DEVICE_TYPE(INTEL_E28F400B,        intel_e28f400b_device)
-DECLARE_DEVICE_TYPE(MACRONIX_29F008TC,     macronix_29f008tc_device)
-DECLARE_DEVICE_TYPE(MACRONIX_29L001MC,     macronix_29l001mc_device)
-DECLARE_DEVICE_TYPE(MACRONIX_29LV160TMC,   macronix_29lv160tmc_device)
-DECLARE_DEVICE_TYPE(MACRONIX_16161616,     macronix_16161616_device)
-DECLARE_DEVICE_TYPE(TMS_29F040,            tms_29f040_device)
+DECLARE_DEVICE_TYPE(INTEL_28F016S5,         intel_28f016s5_device)
+DECLARE_DEVICE_TYPE(SHARP_LH28F016S,        sharp_lh28f016s_device)
+DECLARE_DEVICE_TYPE(SHARP_LH28F016S_16BIT,  sharp_lh28f016s_16bit_device)
+DECLARE_DEVICE_TYPE(ATMEL_29C010,           atmel_29c010_device)
+DECLARE_DEVICE_TYPE(AMD_29F010,             amd_29f010_device)
+DECLARE_DEVICE_TYPE(AMD_29F040,             amd_29f040_device)
+DECLARE_DEVICE_TYPE(AMD_29F080,             amd_29f080_device)
+DECLARE_DEVICE_TYPE(AMD_29F400T,            amd_29f400t_device)
+DECLARE_DEVICE_TYPE(AMD_29F800T,            amd_29f800t_device)
+DECLARE_DEVICE_TYPE(AMD_29F800B_16BIT,      amd_29f800b_16bit_device)
+DECLARE_DEVICE_TYPE(AMD_29LV200T,           amd_29lv200t_device)
+DECLARE_DEVICE_TYPE(AMD_29LV800B_16BIT,     amd_29lv800b_16bit_device)
+DECLARE_DEVICE_TYPE(FUJITSU_29F160TE,       fujitsu_29f160te_device)
+DECLARE_DEVICE_TYPE(FUJITSU_29F016A,        fujitsu_29f016a_device)
+DECLARE_DEVICE_TYPE(FUJITSU_29DL164BD,      fujitsu_29dl164bd_device)
+DECLARE_DEVICE_TYPE(FUJITSU_29LV002TC,      fujitsu_29lv002tc_device)
+DECLARE_DEVICE_TYPE(FUJITSU_29LV800B,       fujitsu_29lv800b_device)
+DECLARE_DEVICE_TYPE(FUJITSU_29F400T_16BIT,  fujitsu_29f400t_16bit_device)
+DECLARE_DEVICE_TYPE(INTEL_E28F400B,         intel_e28f400b_device)
+DECLARE_DEVICE_TYPE(MACRONIX_29F008TC,      macronix_29f008tc_device)
+DECLARE_DEVICE_TYPE(MACRONIX_29L001MC,      macronix_29l001mc_device)
+DECLARE_DEVICE_TYPE(MACRONIX_29LV160TMC,    macronix_29lv160tmc_device)
+DECLARE_DEVICE_TYPE(MACRONIX_29F1610_16BIT, macronix_29F1610_16bit_device)
+DECLARE_DEVICE_TYPE(TMS_29F040,             tms_29f040_device)
 
-DECLARE_DEVICE_TYPE(PANASONIC_MN63F805MNP, panasonic_mn63f805mnp_device)
-DECLARE_DEVICE_TYPE(SANYO_LE26FV10N1TS,    sanyo_le26fv10n1ts_device)
-DECLARE_DEVICE_TYPE(SST_28SF040,           sst_28sf040_device)
-DECLARE_DEVICE_TYPE(SST_39SF040,           sst_39sf040_device)
-DECLARE_DEVICE_TYPE(SST_39VF020,           sst_39vf020_device)
-DECLARE_DEVICE_TYPE(SST_49LF020,           sst_49lf020_device)
+DECLARE_DEVICE_TYPE(PANASONIC_MN63F805MNP,  panasonic_mn63f805mnp_device)
+DECLARE_DEVICE_TYPE(SANYO_LE26FV10N1TS,     sanyo_le26fv10n1ts_device)
+DECLARE_DEVICE_TYPE(SST_28SF040,            sst_28sf040_device)
+DECLARE_DEVICE_TYPE(SST_39SF040,            sst_39sf040_device)
+DECLARE_DEVICE_TYPE(SST_39VF020,            sst_39vf020_device)
+DECLARE_DEVICE_TYPE(SST_49LF020,            sst_49lf020_device)
 
-DECLARE_DEVICE_TYPE(SHARP_LH28F400,        sharp_lh28f400_device)
-DECLARE_DEVICE_TYPE(INTEL_E28F008SA,       intel_e28f008sa_device)
-DECLARE_DEVICE_TYPE(INTEL_TE28F160,        intel_te28f160_device)
-DECLARE_DEVICE_TYPE(SHARP_LH28F160S3,      sharp_lh28f160s3_device)
-DECLARE_DEVICE_TYPE(INTEL_TE28F320,        intel_te28f320_device)
-DECLARE_DEVICE_TYPE(SPANSION_S29GL064S,    spansion_s29gl064s_device)
-DECLARE_DEVICE_TYPE(SHARP_LH28F320BF,      sharp_lh28f320bf_device)
-DECLARE_DEVICE_TYPE(INTEL_28F320J3D,       intel_28f320j3d_device)
-DECLARE_DEVICE_TYPE(INTEL_28F320J5,        intel_28f320j5_device)
-DECLARE_DEVICE_TYPE(INTEL_28F640J5,        intel_28f640j5_device)
-DECLARE_DEVICE_TYPE(SST_39VF400A,          sst_39vf400a_device)
-DECLARE_DEVICE_TYPE(ATMEL_49F4096,         atmel_49f4096_device)
-DECLARE_DEVICE_TYPE(CAT28F020,             cat28f020_device)
-DECLARE_DEVICE_TYPE(TC58FVT800,            tc58fvt800_device)
+DECLARE_DEVICE_TYPE(SHARP_LH28F400,         sharp_lh28f400_device)
+DECLARE_DEVICE_TYPE(INTEL_E28F008SA,        intel_e28f008sa_device)
+DECLARE_DEVICE_TYPE(INTEL_TE28F160,         intel_te28f160_device)
+DECLARE_DEVICE_TYPE(SHARP_LH28F160S3,       sharp_lh28f160s3_device)
+DECLARE_DEVICE_TYPE(INTEL_TE28F320,         intel_te28f320_device)
+DECLARE_DEVICE_TYPE(SPANSION_S29GL064S,     spansion_s29gl064s_device)
+DECLARE_DEVICE_TYPE(SHARP_LH28F320BF,       sharp_lh28f320bf_device)
+DECLARE_DEVICE_TYPE(INTEL_28F320J3D,        intel_28f320j3d_device)
+DECLARE_DEVICE_TYPE(INTEL_28F320J5,         intel_28f320j5_device)
+DECLARE_DEVICE_TYPE(INTEL_28F640J5,         intel_28f640j5_device)
+DECLARE_DEVICE_TYPE(SST_39VF400A,           sst_39vf400a_device)
+DECLARE_DEVICE_TYPE(ATMEL_49F4096,          atmel_49f4096_device)
+DECLARE_DEVICE_TYPE(CAT28F020,              cat28f020_device)
+DECLARE_DEVICE_TYPE(TC58FVT800,             tc58fvt800_device)
 
 #endif // MAME_MACHINE_INTELFSH_H
--- src/mame/webtv/webtv1.cpp
+++ src/mame/webtv/webtv1.cpp
@@ -41,7 +41,8 @@
 #include "main.h"
 #include "screen.h"
 
-#define SYSCLOCK 56000000 // TODO: confirm this is correct
+#define SYSCLOCK             56000000 // TODO: confirm this is correct
+#define EMUCONFIG_4MB_APPROM 0x80
 
 class webtv1_state : public driver_device
 {
@@ -49,17 +50,24 @@ public:
 	// constructor
 	webtv1_state(const machine_config& mconfig, device_type type, const char* tag) :
 		driver_device(mconfig, type, tag),
+		m_sys_config(*this, "sys_config"),
+		m_emu_config(*this, "emu_config"),
 		m_maincpu(*this, "maincpu"),
 		m_spotasic(*this, "spot"),
 		m_serial_id(*this, "serial_id"),
 		m_nvram(*this, "nvram"),
-		m_flash0(*this, "bank0_flash0"), // labeled U0501, contains upper bits
-		m_flash1(*this, "bank0_flash1")  // labeled U0502, contains lower bits
+		m_bank0_1mbit_flash0(*this, "bank0_flash0"), // 2MB approm labeled U0501, contains upper bits
+		m_bank0_1mbit_flash1(*this, "bank0_flash1"), // 2MB approm labeled U0502, contains lower bits
+		m_bank0_2mbit_flash0(*this, "bank0_2mbit_flash0"), // 4MB approm labeled U0501, contains upper bits
+		m_bank0_2mbit_flash1(*this, "bank0_2mbit_flash1"), // 4MB approm labeled U0502, contains lower bits
+		m_bank1_1mbit_flash0(*this, "bank1_flash0"), // 2MB flashable bootrom labeled U0503, contains upper bits
+		m_bank1_1mbit_flash1(*this, "bank1_flash1") // 2MB flashable bootrom labeled U0504, contains lower bits
 	{ }
 	
-	DECLARE_INPUT_CHANGED_MEMBER(pbuff_index_changed);
-	
 	void webtv1_base(machine_config& config);
+
+	void webtv1_bfe(machine_config& config);
+	void webtv1_dbg(machine_config& config);
 	void webtv1_sony(machine_config& config);
 	void webtv1_philips(machine_config& config);
 
@@ -68,43 +76,137 @@ protected:
 	virtual void machine_reset() override;
     
 private:
+	required_ioport m_sys_config;
+	required_ioport m_emu_config;
+
 	required_device<mips3_device> m_maincpu;
 	required_device<spot_asic_device> m_spotasic;
 	required_device<ds2401_device> m_serial_id;
 	required_device<i2cmem_device> m_nvram;
 
-	required_device<amd_29f800b_16bit_device> m_flash0;
-	required_device<amd_29f800b_16bit_device> m_flash1;
-	//required_device<macronix_16161616_device> m_flash0;
-	//required_device<macronix_16161616_device> m_flash1;
+	required_device<amd_29f800b_16bit_device> m_bank0_1mbit_flash0;
+	required_device<amd_29f800b_16bit_device> m_bank0_1mbit_flash1;
+	required_device<macronix_29F1610_16bit_device> m_bank0_2mbit_flash0;
+	required_device<macronix_29F1610_16bit_device> m_bank0_2mbit_flash1;
+	required_device<amd_29f800b_16bit_device> m_bank1_1mbit_flash0;
+	required_device<amd_29lv800b_16bit_device> m_bank1_1mbit_flash1;
 
 	void bank0_flash_w(offs_t offset, uint32_t data);
 	uint32_t bank0_flash_r(offs_t offset);
+	void bank1_flash_w(offs_t offset, uint32_t data);
+	uint32_t bank1_flash_r(offs_t offset);
 
-	void webtv1_map(address_map& map);
+	void webtv1_bfe_map(address_map &map);
+	void webtv1_dbg_map(address_map &map);
+	void webtv1_default_map(address_map &map);
 };
 
+//
+// WebTV stores the approm across 2 flash chips. The approm image is upgradable over a network.
+// 
+// There's two 16-bit flash chips striped across a 32-bit bus. The chip with the upper 16-bits is labeled U0501, and lower is U0502.
+//
+// WebTV supports flash configurations to allow 1MB (2 x 512kbit chips), 2MB (2 x 1Mbit chips) and 4MB (2 x 2Mbit chips) approms.
+// The 1MB flash configuration seemed to never be used, 2MB flash configuration was released to the public
+// and it seemed a 4MB flash configuration was used for debug builds during approm development as well as for prototype Japan builds.
+//
+// 1MB:            AM29F400AT + AM29F400AT (citation needed)
+// Production:     AM29F800BT + AM29F800BT
+// 4MB debug/JP:   MX29F1610  + MX29F1610  (citation needed)
+//
+// WebTV supported SO-44 flash chips:
+//
+// Fujitsu:
+//    MBM29F400B:  bottom bs, 5v 512kbit device id=0x22ab
+//    MBM29F400T:  top bs,    5v 512kbit device id=0x2223 (have MAME support)
+//    MBM29F800B:  bottom bs, 5v 1Mbit   device id=0x2258
+//    MBM29F800T:  top bs,    5v 1Mbit   device id=0x22d6
+//
+// AMD:
+//    AM29F400AB:  bottom bs, 5v 512kbit device id=0x22ab
+//    AM29F400AT:  top bs,    5v 512kbit device id=0x2223
+//    AM29F800BB:  bottom bs, 5v 1Mbit   device id=0x2258 (have MAME support)
+//    AM29F800BT:  top bs,    5v 1Mbit   device id=0x22d6
+//
+// Macronix:
+//    MX29F1610:   top bs,    5v 2Mbit   device id=0x00f1 (have MAME support)
+//
+// NOTE: if you dump these chips you may need to byte swap the output. The bus is big-endian.
+//
 void webtv1_state::bank0_flash_w(offs_t offset, uint32_t data)
 {
-	// WebTV FCS uses two AMD AM29F800BT chips on the board for storing its software.
-	// One chip is for the lower 16 bits (labeled U0502), and the other is for the upper 16 bits (labeled U0501).
-
-	uint16_t upper_value = (data >> 16) & 0xffff;
-	m_flash0->write(offset, upper_value);
+	uint16_t upper_value = (data >> 0x10) & 0xffff;
 	
 	uint16_t lower_value = data & 0xffff;
-	m_flash1->write(offset, lower_value);
+	
+	if(m_emu_config->read() & EMUCONFIG_4MB_APPROM)
+	{
+		m_bank0_2mbit_flash0->write(offset, upper_value); // bank0_2mbit_flash0 file
+		m_bank0_2mbit_flash1->write(offset, lower_value); // bank0_2mbit_flash1 file
+	}
+	else
+	{
+		m_bank0_1mbit_flash0->write(offset, upper_value); // bank0_flash0 file
+		m_bank0_1mbit_flash1->write(offset, lower_value); // bank0_flash1 file
+	}
 }
 
 uint32_t webtv1_state::bank0_flash_r(offs_t offset)
 {
-	uint16_t upper_value = m_flash0->read(offset);
-	uint16_t lower_value = m_flash1->read(offset);
+	uint16_t upper_value = 0x0;
+	uint16_t lower_value = 0x0;
+	
+	if(m_emu_config->read() & EMUCONFIG_4MB_APPROM)
+	{
+		upper_value = m_bank0_2mbit_flash0->read(offset); // bank0_2mbit_flash0 file
+		lower_value = m_bank0_2mbit_flash1->read(offset); // bank0_2mbit_flash1 file
+	}
+	else
+	{
+		upper_value = m_bank0_1mbit_flash0->read(offset); // bank0_flash0 file
+		lower_value = m_bank0_1mbit_flash1->read(offset); // bank0_flash1 file
+	}
 
     return (upper_value << 16) | (lower_value);
 }
 
-void webtv1_state::webtv1_map(address_map &map)
+//
+// WebTV stores the bootrom across 2 chips. Production boxes use WebTV-branded mask ROM chips and development boxes use flash.
+// 
+// WebTV has an unusual configuration. There's two 16-bit chips striped across a 32-bit bus.
+// The chip with the upper 16-bits is labeled U0503, and lower is U0504. In addition, U0503 is a 3v chip and U0504 is 5v.
+//
+// The bootrom always seems to be 2MB.
+//
+// From our understanding the flash-based bootroms use AMD SO-44 chips (citation needed):
+//
+// AMD:
+//    AM29F800BB:  bottom bs, 5v 1Mbit   device id=0x2258 (have MAME support)
+//    AM29F800BT:  top bs,    5v 1Mbit   device id=0x22d6
+//    AM29LV800BB: bottom bs, 3v 1Mbit   device id=0x225b
+//    AM29LV800BT: top bs,    3v 1Mbit   device id=0x22da (have MAME support)
+//
+// NOTE: if you dump these chips you may need to byte swap the output. The bus is big-endian.
+//
+void webtv1_state::bank1_flash_w(offs_t offset, uint32_t data)
+{
+	uint16_t upper_value = (data >> 0x10) & 0xffff;
+	uint16_t lower_value = data & 0xffff;
+
+	m_bank1_1mbit_flash0->write(offset, upper_value);
+	m_bank1_1mbit_flash1->write(offset, lower_value);
+}
+
+uint32_t webtv1_state::bank1_flash_r(offs_t offset)
+{
+	uint16_t upper_value = m_bank1_1mbit_flash0->read(offset);
+	uint16_t lower_value = m_bank1_1mbit_flash1->read(offset);
+
+    return (upper_value << 16) | (lower_value);
+}
+
+
+void webtv1_state::webtv1_bfe_map(address_map &map)
 {
 	map.global_mask(0x1fffffff);
 
@@ -120,8 +222,49 @@ void webtv1_state::webtv1_map(address_map &map)
 	map(0x04005000, 0x04005fff).m(m_spotasic, FUNC(spot_asic_device::mem_unit_map));
 
 	// ROM
-	map(0x1f000000, 0x1f3fffff).rw(FUNC(webtv1_state::bank0_flash_r), FUNC(webtv1_state::bank0_flash_w)).share("bank0"); // Flash ROM, 4MB (retail configuration 2MB)
-	map(0x1f800000, 0x1fffffff).rom().region("bank1", 0); // Mask ROM
+	// Technically inside the bank1 region
+	map(0x1fe00000, 0x1fffffff).rw(FUNC(webtv1_state::bank0_flash_r), FUNC(webtv1_state::bank0_flash_w)).share("approm_flash");
+	map(0x1f800000, 0x1fdfffff).rom().region("bootrom_mask", 0);
+}
+
+void webtv1_state::webtv1_dbg_map(address_map &map)
+{
+	map.global_mask(0x1fffffff);
+
+	// RAM
+	map(0x00000000, 0x007fffff).ram().share("ram"); // 8MB is not accurate to retail hardware! Ideally this would be 2MB or 4MB, mirrored across this memory space
+
+	// SPOT
+	map(0x04000000, 0x04000fff).m(m_spotasic, FUNC(spot_asic_device::bus_unit_map));
+	map(0x04001000, 0x04001fff).m(m_spotasic, FUNC(spot_asic_device::rom_unit_map));
+	map(0x04002000, 0x04002fff).m(m_spotasic, FUNC(spot_asic_device::aud_unit_map));
+	map(0x04003000, 0x04003fff).m(m_spotasic, FUNC(spot_asic_device::vid_unit_map));
+	map(0x04004000, 0x04004fff).m(m_spotasic, FUNC(spot_asic_device::dev_unit_map));
+	map(0x04005000, 0x04005fff).m(m_spotasic, FUNC(spot_asic_device::mem_unit_map));
+
+	// ROM
+	map(0x1f000000, 0x1f3fffff).rw(FUNC(webtv1_state::bank0_flash_r), FUNC(webtv1_state::bank0_flash_w)).share("approm_flash");
+	map(0x1f800000, 0x1fdfffff).rw(FUNC(webtv1_state::bank1_flash_r), FUNC(webtv1_state::bank1_flash_w)).share("bootrom_flash");
+}
+
+void webtv1_state::webtv1_default_map(address_map &map)
+{
+	map.global_mask(0x1fffffff);
+
+	// RAM
+	map(0x00000000, 0x007fffff).ram().share("ram"); // 8MB is not accurate to retail hardware! Ideally this would be 2MB or 4MB, mirrored across this memory space
+
+	// SPOT
+	map(0x04000000, 0x04000fff).m(m_spotasic, FUNC(spot_asic_device::bus_unit_map));
+	map(0x04001000, 0x04001fff).m(m_spotasic, FUNC(spot_asic_device::rom_unit_map));
+	map(0x04002000, 0x04002fff).m(m_spotasic, FUNC(spot_asic_device::aud_unit_map));
+	map(0x04003000, 0x04003fff).m(m_spotasic, FUNC(spot_asic_device::vid_unit_map));
+	map(0x04004000, 0x04004fff).m(m_spotasic, FUNC(spot_asic_device::dev_unit_map));
+	map(0x04005000, 0x04005fff).m(m_spotasic, FUNC(spot_asic_device::mem_unit_map));
+
+	// ROM
+	map(0x1f000000, 0x1f3fffff).rw(FUNC(webtv1_state::bank0_flash_r), FUNC(webtv1_state::bank0_flash_w)).share("approm_flash");
+	map(0x1f800000, 0x1fdfffff).rom().region("bootrom_mask", 0);
 }
 
 void webtv1_state::webtv1_base(machine_config &config)
@@ -131,12 +274,18 @@ void webtv1_state::webtv1_base(machine_config &config)
 	R4640BE(config, m_maincpu, SYSCLOCK*2);
 	m_maincpu->set_icache_size(0x2000);
 	m_maincpu->set_dcache_size(0x2000);
-	m_maincpu->set_addrmap(AS_PROGRAM, &webtv1_state::webtv1_map);
 
-	AMD_29F800B_16BIT(config, m_flash0, 0);
-	AMD_29F800B_16BIT(config, m_flash1, 0);
-	//MACRONIX_16161616(config, m_flash0, 0);
-	//MACRONIX_16161616(config, m_flash1, 0);
+	// 2MB bf0app Prod:
+	AMD_29F800B_16BIT(config, m_bank0_1mbit_flash0, 0);
+	AMD_29F800B_16BIT(config, m_bank0_1mbit_flash1, 0);
+
+	// 4MB Debug:
+	MACRONIX_29F1610_16BIT(config, m_bank0_2mbit_flash0, 0);
+	MACRONIX_29F1610_16BIT(config, m_bank0_2mbit_flash1, 0);
+
+	// 2MB flashable bootrom
+	AMD_29F800B_16BIT(config, m_bank1_1mbit_flash0, 0);
+	AMD_29LV800B_16BIT(config, m_bank1_1mbit_flash1, 0);
 
 	DS2401(config, m_serial_id, 0);
 
@@ -150,16 +299,34 @@ void webtv1_state::webtv1_base(machine_config &config)
 	m_spotasic->set_nvram(m_nvram);
 }
 
+void webtv1_state::webtv1_bfe(machine_config& config)
+{
+	webtv1_base(config);
+
+	m_maincpu->set_addrmap(AS_PROGRAM, &webtv1_state::webtv1_bfe_map);
+}
+
+void webtv1_state::webtv1_dbg(machine_config& config)
+{
+	webtv1_base(config);
+
+	m_maincpu->set_addrmap(AS_PROGRAM, &webtv1_state::webtv1_dbg_map);
+}
+
 void webtv1_state::webtv1_sony(machine_config& config)
 {
 	// manufacturer is determined by the contents of DS2401
 	webtv1_base(config);
+
+	m_maincpu->set_addrmap(AS_PROGRAM, &webtv1_state::webtv1_default_map);
 }
 
 void webtv1_state::webtv1_philips(machine_config& config)
 {
 	// manufacturer is determined by the contents of DS2401
 	webtv1_base(config);
+
+	m_maincpu->set_addrmap(AS_PROGRAM, &webtv1_state::webtv1_default_map);
 }
 
 void webtv1_state::machine_start()
@@ -170,11 +337,6 @@ void webtv1_state::machine_start()
 void webtv1_state::machine_reset()
 {
 
-}
-
-INPUT_CHANGED_MEMBER(webtv1_state::pbuff_index_changed)
-{
-	m_spotasic->pixel_buffer_index_update();
 }
 
 // Sysconfig options are usually configured via resistors on the board.
@@ -250,7 +412,7 @@ static INPUT_PORTS_START( sys_config )
 
 	PORT_DIPNAME(0x8000000, 0x8000000, "Bank 1 ROM type")
 	PORT_DIPSETTING(0x0000000, "Flash ROM")
-	PORT_DIPSETTING(0x8000000, "Mask ROM (emulated behavior)")
+	PORT_DIPSETTING(0x8000000, "Mask ROM")
 	
 	PORT_DIPNAME(0x30000000, 0x20000000, "Bank 0 ROM speed")
 	PORT_DIPSETTING(0x00000000, "200ns/100ns")
@@ -287,6 +449,10 @@ static INPUT_PORTS_START( emu_config )
 	PORT_CONFNAME(0x20, 0x20, "Interrupts enabled")
 	PORT_CONFSETTING(0x00, "No")
 	PORT_CONFSETTING(0x20, "Yes")
+
+	PORT_CONFNAME(0xc0, 0x00, "Approm flash configuration")
+	PORT_CONFSETTING(0x00, "2MB approm (bank0_flash0/1)")
+	PORT_CONFSETTING(0x80, "4MB approm (bank0_2mbit_flash0/1)")
 INPUT_PORTS_END
 
 static INPUT_PORTS_START( webtv1_input )
@@ -294,27 +460,46 @@ static INPUT_PORTS_START( webtv1_input )
 	PORT_INCLUDE(emu_config)
 INPUT_PORTS_END
 
+ROM_START( wtv1bfe )
+	ROM_REGION(0x8, "serial_id", 0)     /* Electronic Serial DS2401 */
+	ROM_LOAD("ds2401.bin", 0x0000, 0x0008, NO_DUMP)
+
+	ROM_REGION32_BE(0x600000, "bootrom_mask", 0)
+	ROM_SYSTEM_BIOS(0, "bfe-boot", "Prototype bfe BootROM (1.0, build 105)")
+	ROMX_LOAD("bfe-boot.o", 0x400000, 0x200000, NO_DUMP, ROM_BIOS(0))         // 0x1fc00000/0x9fc00000/0xbfc00000 (bootrom base)
+	ROM_SYSTEM_BIOS(1, "prealpha-boot", "Pre-alpha bfe BootROM")
+	ROMX_LOAD("prealpha-boot.o", 0x400000, 0x200000, NO_DUMP, ROM_BIOS(1))    // 0x1fc00000/0x9fc00000/0xbfc00000 (bootrom base)
+ROM_END
+
+ROM_START( wtv1dbg )
+	ROM_REGION(0x8, "serial_id", 0)     /* Electronic Serial DS2401 */
+	ROM_LOAD("ds2401.bin", 0x0000, 0x0008, NO_DUMP)
+
+	ROM_REGION32_BE(0x600000, "bootrom_mask", 0)
+	ROM_SYSTEM_BIOS(0, "bootrom", "Standard bf0 BootROM (1.0, build 105)")
+	ROMX_LOAD("bootrom.o", 0x400000, 0x200000, NO_DUMP, ROM_BIOS(0))          // 0x1fc00000/0x9fc00000/0xbfc00000 (bootrom base)
+ROM_END
+
 ROM_START( wtv1sony )
 	ROM_REGION(0x8, "serial_id", 0)     /* Electronic Serial DS2401 */
 	ROM_LOAD("ds2401.bin", 0x0000, 0x0008, NO_DUMP)
 
-	ROM_REGION32_BE(0x800000, "bank1", 0)
-	ROM_LOAD("bootrom.o", 0x000000, 0x200000, NO_DUMP) /* pre-decoded; from archival efforts of the WebTV update servers */
-	ROM_RELOAD(0x200000, 0x200000)
-	ROM_RELOAD(0x400000, 0x200000)
-	ROM_RELOAD(0x600000, 0x200000)
+	ROM_REGION32_BE(0x600000, "bootrom_mask", 0)
+	ROM_SYSTEM_BIOS(0, "bootrom", "Standard bf0 BootROM (1.0, build 105)")
+	ROMX_LOAD("bootrom.o", 0x400000, 0x200000, NO_DUMP, ROM_BIOS(0))          // 0x1fc00000/0x9fc00000/0xbfc00000 (bootrom base)
 ROM_END
 
 ROM_START( wtv1phil )
 	ROM_REGION(0x8, "serial_id", 0)     /* Electronic Serial DS2401 */
 	ROM_LOAD("ds2401.bin", 0x0000, 0x0008, NO_DUMP)
 
-	ROM_REGION32_BE(0x800000, "bank1", 0)
-	ROM_LOAD("bootrom.o", 0x000000, 0x200000, NO_DUMP) /* pre-decoded; from archival efforts of the WebTV update servers */
-	ROM_RELOAD(0x200000, 0x200000)
-	ROM_RELOAD(0x400000, 0x200000)
+	ROM_REGION32_BE(0x600000, "bootrom_mask", 0)
+	ROM_SYSTEM_BIOS(0, "bootrom", "Standard bf0 BootROM (1.0, build 105)")
+	ROMX_LOAD("bootrom.o", 0x400000, 0x200000, NO_DUMP, ROM_BIOS(0))          // 0x1fc00000/0x9fc00000/0xbfc00000 (bootrom base)
 ROM_END
 
 //    YEAR  NAME      PARENT  COMPAT  MACHINE         INPUT         CLASS         INIT        COMPANY               FULLNAME                            FLAGS
-CONS( 1996, wtv1sony,      0,      0, webtv1_sony,    webtv1_input, webtv1_state, empty_init, "Sony",               "INT-W100 WebTV Internet Terminal", MACHINE_NOT_WORKING + MACHINE_NO_SOUND )
-CONS( 1996, wtv1phil,      0,      0, webtv1_philips, webtv1_input, webtv1_state, empty_init, "Philips-Magnavox",   "MAT960 WebTV Internet Terminal",   MACHINE_NOT_WORKING + MACHINE_NO_SOUND )
+CONS( 1996, wtv1bfe,  0,      0,      webtv1_bfe,     webtv1_input, webtv1_state, empty_init, "WebTV",              "WebTV bfeapp Protoype Box",        MACHINE_UNOFFICIAL + MACHINE_IS_INCOMPLETE + MACHINE_IMPERFECT_TIMING + MACHINE_IMPERFECT_SOUND )
+CONS( 1996, wtv1dbg,  0,      0,      webtv1_dbg,     webtv1_input, webtv1_state, empty_init, "WebTV",              "WebTV 4MB Approm Box",             MACHINE_UNOFFICIAL + MACHINE_IS_INCOMPLETE + MACHINE_IMPERFECT_TIMING + MACHINE_IMPERFECT_SOUND )
+CONS( 1996, wtv1sony, 0,      0,      webtv1_sony,    webtv1_input, webtv1_state, empty_init, "Sony",               "INT-W100 WebTV Internet Terminal", MACHINE_UNOFFICIAL + MACHINE_IS_INCOMPLETE + MACHINE_IMPERFECT_TIMING + MACHINE_IMPERFECT_SOUND )
+CONS( 1996, wtv1phil, 0,      0,      webtv1_philips, webtv1_input, webtv1_state, empty_init, "Philips-Magnavox",   "MAT960 WebTV Internet Terminal",   MACHINE_UNOFFICIAL + MACHINE_IS_INCOMPLETE + MACHINE_IMPERFECT_TIMING + MACHINE_IMPERFECT_SOUND )
