84d929ddfc09381d8f4f4fc28ee40bc3f43f4e35
wtvemac <wtvemac@proton.me>
2025-06-16 08:47:56 -0400

    Add tuner device
    Right now, this is just used so the correct I2C responses are heard. I have no plans on fully implementing any tuners.

---
+++ src/mame/webtv/wtvtuner.h
@@ -0,0 +1,54 @@
+
+#ifndef MAME_MACHINE_WTVTUNER_H
+#define MAME_MACHINE_WTVTUNER_H
+
+#pragma once
+
+#include "machine/i2chle.h"
+
+class wtvtuner_device_base : public device_t, public i2c_hle_interface
+{
+
+public:
+
+	wtvtuner_device_base(const machine_config &mconfig, device_type type, const char *tag, device_t *owner, uint32_t clock, u16 iic_address);
+
+	int sda_read();
+
+protected:
+
+	virtual void device_start() override ATTR_COLD;
+	virtual void device_reset() override ATTR_COLD;
+
+private:
+
+	int m_sdar;
+
+	void sdar_w(int state);
+
+};
+
+class l64734_tuner_device : public wtvtuner_device_base
+{
+
+public:
+
+	l64734_tuner_device(const machine_config &mconfig, const char *tag, device_t *owner, u16 iic_address, uint32_t clock = 0);
+
+protected:
+
+	virtual void device_start() override ATTR_COLD;
+	virtual void device_reset() override ATTR_COLD;
+
+	virtual u8 read_data(u16 offset) override;
+	virtual void write_data(u16 offset, u8 data) override;
+
+private:
+
+	uint8_t m_chip_id;
+
+};
+
+DECLARE_DEVICE_TYPE(L64734, l64734_tuner_device)
+
+#endif // MAME_MACHINE_WTVTUNER_H
