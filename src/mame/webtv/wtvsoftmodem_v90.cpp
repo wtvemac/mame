ff5546234e4bb3c07a47ebca9eca14c681e21f81
wtvemac <wtvemac@proton.me>
2025-12-19 21:52:50 -0500

    Softmodem V34 and V90 boilerplate

---
+++ src/mame/webtv/wtvsoftmodem_v90.cpp
@@ -0,0 +1,27 @@
+// license: BSD-3-Clause
+// copyright-holders: wtvemac
+
+// Description here
+
+#include "wtvsoftmodem_v90.h"
+#include <cstdio>
+
+wtvsoftmodem_v90::wtvsoftmodem_v90() :
+	wtvsoftmodem_fsk()
+{
+	wtvsoftmodem_fsk::set_sample_rate(wtvsoftmodem_dsp::DEFAULT_SAMPLE_RATE);
+
+	wtvsoftmodem_fsk::init(wtvsoftmodem_fsk::FSK_V23_MODE2);
+
+	wtvsoftmodem_fsk::set_tx_enabled(false);
+}
+
+void wtvsoftmodem_v90::push_rx_bit(bool bit)
+{
+	//
+}
+
+bool wtvsoftmodem_v90::pop_tx_bit()
+{
+	return 0;
+}
