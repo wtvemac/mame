22233e7756be1c2f7de40386d42d460d56f98d74
wtvemac <wtvemac@proton.me>
2026-07-21 04:29:36 -0400

    Better option to get cache-line size on Linux/Unix
    It was hitting the file I/O stack every time the DRC compiled new code. This was incredibly costly on performance. So I'm trying a sysconf() first and fallback to the slow file I/O if sysconf is unavailable at compile time.

--- src/osd/modules/lib/osdlib_unix.cpp
+++ src/osd/modules/lib/osdlib_unix.cpp
@@ -121,7 +121,13 @@ void osd_break_into_debugger(const char *message)
 
 std::pair<std::error_condition, unsigned> osd_get_cache_line_size() noexcept
 {
-#if defined(__linux__)
+#if defined(_SC_LEVEL1_DCACHE_LINESIZE)
+    long size = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
+    if (size > 0)
+        return std::make_pair(std::error_condition(), static_cast<unsigned>(size));
+	else
+		return std::make_pair(std::errc::not_supported, 0U);
+#elif defined(__linux__)
 	FILE *const f = std::fopen("/sys/devices/system/cpu/cpu0/cache/index0/coherency_line_size", "r");
 	if (!f)
 		return std::make_pair(std::error_condition(errno, std::generic_category()), 0U);
